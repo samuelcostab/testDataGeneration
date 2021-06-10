cb8a4f9fd427d9487363a9c0ad5568876ed1cab6
Samuel Costa <samuelufcbrito@gmail.com>
2021-06-10 20:56:04 -0300

    Libs vitais para execução

--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
 
 import ast
 import inspect
+import sys
+import importlib
 from geraDot import *
 from grafo import Grafo
 from ast_walker import *
@@ -13,7 +15,7 @@ from interpreter import dataGenerator
 def runner(nomeFunc, mainString, testResult, fun_name):
     # Executa tudo no arquivo executeTest.py com o comando "python3 executeTest.py"
     # Ao analisar nova função, lembrar de alterar o nome do arquivo em runner.py e executeTest.py
-    # ao gerar o arquivo g1.dot, lembrar de retirar o primeiro nó de "declaração" para não entrar no loop infinito
+    # Ao gerar o arquivo g1.dot, lembrar de retirar o primeiro nó de "declaração" para não entrar no loop infinito
     grafo = Grafo()
     codeAst = ast.parse(inspect.getsource(nomeFunc))
     
